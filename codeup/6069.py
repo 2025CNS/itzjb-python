2fb47c0ff45c1aa818bb336ad37ec6c83c33d20f
itzjb <146229370+itzjb@users.noreply.github.com>
2025-03-18 23:03:31 +0900

    codeup 6069
    6069

---
+++ codeup/6069.py
@@ -0,0 +1,11 @@
+t = input()
+if t == "A":
+    print("best!!!")
+elif t == "B":
+    print("good!!")
+elif t == "C":
+    print("run!")
+elif t == "D":
+    print("slowly~")
+else:
+    print("what?")
