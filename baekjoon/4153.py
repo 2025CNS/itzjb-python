e2ba1b7cb1947f78bbf81e0a0f6e38d8f59421cb
itzjb <146229370+itzjb@users.noreply.github.com>
2025-05-16 11:29:52 +0900

    boj 4153

---
+++ baekjoon/4153.py
@@ -0,0 +1,10 @@
+while 1:
+    lst = list(map(int, input().split()))
+    if sum(lst) == 0:
+        break
+    
+    lst.sort()
+    if lst[0]**2 + lst[1]**2 == lst[2]**2:
+        print("right")
+    else:
+        print("wrong")
